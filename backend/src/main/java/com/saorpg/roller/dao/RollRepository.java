b657821c9bdce41a80eb00df5c11a171bca9b858
Juhmer Tena <48825479+Juhmer-Tena@users.noreply.github.com>
2025-03-31 07:56:21 +0000

    refactor: remove unused function in RollRepository

--- backend/src/main/java/com/saorpg/roller/dao/RollRepository.java
+++ backend/src/main/java/com/saorpg/roller/dao/RollRepository.java
@@ -1,15 +1,10 @@
 package com.saorpg.roller.dao;
 
-import java.util.List;
-
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.saorpg.roller.domain.Roll;
-import com.saorpg.roller.domain.RollMetadata;
 
 @Repository
 public interface RollRepository extends JpaRepository<Roll, Integer> {
-  List<Roll> findAllByMetadata(Example<RollMetadata> metadata);
 }
