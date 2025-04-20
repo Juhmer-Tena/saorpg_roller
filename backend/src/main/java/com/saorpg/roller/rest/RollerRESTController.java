ab8e392713142b24e9d9d432de74a62936ce9dfc
Juhmer Tena <48825479+Juhmer-Tena@users.noreply.github.com>
2025-04-20 02:18:59 +0000

    feature: use nginx as a reverse proxy
    Client-sided calls from the frontend web pages to the backend may fail
    or not work as expected due to cors and security modes. Using a reverse
    proxy allows the backend and frontend to originate from the same host
    and port but using different paths.

--- backend/src/main/java/com/saorpg/roller/rest/RollerRESTController.java
+++ backend/src/main/java/com/saorpg/roller/rest/RollerRESTController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,7 +21,6 @@ import com.saorpg.roller.domain.RollMetadata;
 import com.saorpg.roller.dto.RollRequest;
 
 @RestController
-@RequestMapping("api")
 public class RollerRESTController {
   @Value("${com.saorpg.roller.versionRef}")
   private String VERSION;
---
+++ infrastructure/src/nginx/templates/README.md
@@ -0,0 +1,3 @@
+# Nginx templates
+
+These templates are added to the http directive of the default nginx configuration for Docker.
