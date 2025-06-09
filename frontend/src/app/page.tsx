5d337b2203ae2e777bac6ca15c151bc87010f6c7
Juhmer Tena <48825479+Juhmer-Tena@users.noreply.github.com>
2025-06-09 09:50:08 +0000

    refactor: use Link from Next.js instead of anchor
    Link tags from Next.js provides capabilities such as prefetching and
    client-sided navigation between Next.js routes. This change is more to
    abide by Next.js ecosystem rules instead of an actual functional change.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { RollCharacterFilter, RollIDFilter, RollPostFilter } from "@/components/RollerFilters";
 import { RollerForm } from "@/components/RollerForm";
 import RollTable from "@/components/RollTable";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -55,7 +56,7 @@ export default function Home() {
                           <div
                             className="tooltip tooltip-top grow"
                           >
-                            <a
+                            <Link
                               href="https://discord.gg/p2zsGGJ"
                               target="_blank"
                             >
@@ -71,7 +72,7 @@ export default function Home() {
                                   <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
                                 </svg>
                               </label>
-                            </a>
+                            </Link>
                             <div className="tooltip-content text-xs">
                               Join our Discord!
                             </div>
@@ -81,7 +82,7 @@ export default function Home() {
                           <div
                             className="tooltip tooltip-top grow"
                           >
-                            <a href="https://www.sao-rpg.com/" target="_blank">
+                            <Link href="https://www.sao-rpg.com/" target="_blank">
                               <label className="btn btn-xs btn-ghost justify-start">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
@@ -94,7 +95,7 @@ export default function Home() {
                                   <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001" />
                                 </svg>
                               </label>
-                            </a>
+                            </Link>
                             <div className="tooltip-content text-xs">
                               Return to SAO-RPG
                             </div>
