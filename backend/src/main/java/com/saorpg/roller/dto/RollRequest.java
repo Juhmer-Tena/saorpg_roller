ecb91c5df1b72cc80af54c1e634a07d6bc209270
Juhmer Tena <48825479+Juhmer-Tena@users.noreply.github.com>
2025-12-26 09:17:31 +0000

    fix: allow requested number of rolls to be null

--- backend/src/main/java/com/saorpg/roller/dto/RollRequest.java
+++ backend/src/main/java/com/saorpg/roller/dto/RollRequest.java
@@ -1,4 +1,4 @@
 package com.saorpg.roller.dto;
 
-public record RollRequest(String postLink, String purpose, String character, int numberOfRolls) {
+public record RollRequest(String postLink, String purpose, String character, Integer numberOfRolls) {
 }
